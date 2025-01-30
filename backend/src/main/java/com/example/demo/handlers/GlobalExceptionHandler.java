8a61fb95211b8d12bf50a1694a89e0921d45ab08
Arda Alemdaroglu <arda.alemdaroglu@gmail.com>
2025-01-31 00:13:41 +0300

    add a handler for bad credentials exception

--- backend/src/main/java/com/example/demo/handlers/GlobalExceptionHandler.java
+++ backend/src/main/java/com/example/demo/handlers/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authorization.AuthorizationDeniedException;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -41,6 +42,11 @@ public class GlobalExceptionHandler {
         logger.error("Illegal argument error: {}", ex.getMessage(), ex);
 
         return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<String> handleBadCredentialsException(BadCredentialsException ex) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Bad credentials");
     }
 
     @ExceptionHandler(AuthorizationDeniedException.class)
